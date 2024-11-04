8678d1ef33ca663b766f02e6871a61c454ca9af7
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-04 23:52:39 +0000

    Removed current track info from song description

--- MusicMakerRandomize/Scripts/AudioLogic.lua
+++ MusicMakerRandomize/Scripts/AudioLogic.lua
@@ -4,30 +4,24 @@ ModUtil.Path.Wrap("MusicianMusic", function(base, args)
 	-- If the chosen song is Song_RandomSong and the Music Maker not already playing something, choose a random track and save it in the currentRun to reset it only after a run ends
 	if args.TrackName == "Song_RandomSongTrack" then
 		local availableTracks = {}
-		-- Choose from all available tracks (even those not yet unlocked/visible to the player)
 		if MusicMakerRandomize.Config.AllSongs then
+			-- Choose from all available tracks (even those not yet unlocked/visible to the player)
 			availableTracks = DeepCopyTable(ScreenData.MusicPlayer.Songs)
-			-- Choose only from unlocked tracks
 		elseif GameState and GameState.WorldUpgrades then
+			-- Choose only from unlocked tracks
 			for _, songName in ipairs(ScreenData.MusicPlayer.Songs) do
 				if songName ~= "Song_RandomSong" and GameState.WorldUpgrades[songName] then
-					table.insert(availableTracks, WorldUpgradeData[songName])
+					table.insert(availableTracks, WorldUpgradeData[songName].TrackName)
 				end
 			end
 		end
 
 		-- Choose a random song from the available ones, if no other song was chosen yet in this "run"
 		if CurrentRun.MusicMakerRandomizeTrackName == nil then
-			local chosenTrack = availableTracks[RandomInt(1, #availableTracks)]
-			CurrentRun.MusicMakerRandomizeTrackName = chosenTrack.TrackName
-			-- To update the description of the Song_RandomSong to include the friendly name of the chosen track
-			GameState.MusicMakerRandomizeMusicPlayerSongName = chosenTrack.Name
+			CurrentRun.MusicMakerRandomizeTrackName = availableTracks[RandomInt(1, #availableTracks)]
 		end
 
 		args.TrackName = CurrentRun.MusicMakerRandomizeTrackName
-	else
-		-- Include the currently playing, manually selected song as currently playing song in the description
-		GameState.MusicMakerRandomizeMusicPlayerSongName = GameState.MusicPlayerSongName
 	end
 
 	base(args)
