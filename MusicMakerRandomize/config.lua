620fe57a75593b1f46cdac7c70068506cd89a123
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-11-05 18:10:17 +0100

    Added Thunderstore compatibility through DemonDaemon (#1)

--- MusicMakerRandomize/config.lua
+++ MusicMakerRandomize/config.lua
@@ -1,10 +1,11 @@
-local mod = ModUtil.Mod.Register("MusicMakerRandomizer")
-
--- After making changes here, you need to re-run modimporter.exe
-mod.Config = {
+-- After making changes here, you need to re-run modimporter.exe (if not using Thunderstore)
+local config = {
 	-- Simply set this to false to disable the mod.
 	Enabled = true,
 	-- SPOILER WARNING: If this is set to true, random songs will be chosen from all available tracks.
 	-- This includes those you may not even know exist (e.g. from future regions or bosses).
 	AllSongs = false
 }
+
+MusicMakerRandomizer.Config = config
+return config
