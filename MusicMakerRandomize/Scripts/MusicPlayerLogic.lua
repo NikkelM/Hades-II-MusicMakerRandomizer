9e8c3347942bf00506a11c524712b790ed8b9b91
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-05 11:52:47 +0000

    Do not show what song is playing if the selected song is not the random song

--- MusicMakerRandomize/Scripts/MusicPlayerLogic.lua
+++ MusicMakerRandomize/Scripts/MusicPlayerLogic.lua
@@ -7,14 +7,17 @@ ModUtil.Path.Wrap("SelectMusicPlayerItem", function(base, screen, button)
 
 	base(screen, button)
 
-	-- Update the description with the new currently playing song, or "Nothing..." if the player paused the random song
-	local components = button.Screen.Components
-	ModifyTextBox(
-		{
-			Id = components.InfoBoxDescription.Id,
-			Text = button.Data.Name,
-			UseDescription = true,
-			LuaKey = "TempTextData",
-			LuaValue = { MusicMakerRandomizeFriendlyPlayingSong = GameState.MusicMakerRandomizeFriendlyPlayingSong }
-		})
+	if (GameState.MusicPlayerSongName == "Song_RandomSong" or GameState.MusicPlayerSongName == nil) and button.Data.Name == "Song_RandomSong" then
+		-- Update the description with the new currently playing song, or "Nothing..." if the player paused the random song
+		local components = button.Screen.Components
+		ModifyTextBox(
+			{
+				Id = components.InfoBoxDescription.Id,
+				Text = "Song_RandomSong_PlayingInfo",
+				UseDescription = true,
+				LuaKey = "TempTextData",
+				LuaValue = { MusicMakerRandomizeFriendlyPlayingSong = GameState.MusicMakerRandomizeFriendlyPlayingSong }
+			}
+		)
+	end
 end)
--- MusicMakerRandomize/Scripts/MusicPlayerPresentation.lua
+++ MusicMakerRandomize/Scripts/MusicPlayerPresentation.lua
@@ -3,14 +3,25 @@ if not MusicMakerRandomize.Config.Enabled then return end
 ModUtil.Path.Wrap("MouseOverMusicPlayerItem", function(base, button)
 	base(button)
 
-	-- Update the description with the currently playing song when the random song is first selected
 	local components = button.Screen.Components
-	ModifyTextBox(
-		{
-			Id = components.InfoBoxDescription.Id,
-			Text = button.Data.Name,
-			UseDescription = true,
-			LuaKey = "TempTextData",
-			LuaValue = { MusicMakerRandomizeFriendlyPlayingSong = GameState.MusicMakerRandomizeFriendlyPlayingSong }
-		})
+	-- If the random song has been bought and is currently playing, update the description with the currently playing song
+	if GameState.WorldUpgrades["Song_RandomSong"] and GameState.MusicPlayerSongName == "Song_RandomSong" and button.Data.Name == "Song_RandomSong" then
+		ModifyTextBox(
+			{
+				Id = components.InfoBoxDescription.Id,
+				Text = "Song_RandomSong_PlayingInfo",
+				UseDescription = true,
+				LuaKey = "TempTextData",
+				LuaValue = { MusicMakerRandomizeFriendlyPlayingSong = GameState.MusicMakerRandomizeFriendlyPlayingSong }
+			}
+		)
+	else
+		ModifyTextBox(
+			{
+				Id = components.InfoBoxDescription.Id,
+				Text = button.Data.Name,
+				UseDescription = true,
+			}
+		)
+	end
 end)
