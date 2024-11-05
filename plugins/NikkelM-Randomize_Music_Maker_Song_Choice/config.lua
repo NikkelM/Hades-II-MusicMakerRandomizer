0ebafc18e2b3363fa61d652345fe9d8e9a3e40fd
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-05 15:35:44 +0000

    Migrate to work with Thunderstore/Hell2Modding

--- plugins/NikkelM-Randomize_Music_Maker_Song_Choice/config.lua
+++ plugins/NikkelM-Randomize_Music_Maker_Song_Choice/config.lua
@@ -1,10 +1,10 @@
-local mod = ModUtil.Mod.Register("MusicMakerRandomizer")
-
 -- After making changes here, you need to re-run modimporter.exe
-mod.Config = {
+local config = {
 	-- Simply set this to false to disable the mod.
 	Enabled = true,
 	-- SPOILER WARNING: If this is set to true, random songs will be chosen from all available tracks.
 	-- This includes those you may not even know exist (e.g. from future regions or bosses).
 	AllSongs = false
 }
+MusicMakerRandomizer.config = config
+return config
