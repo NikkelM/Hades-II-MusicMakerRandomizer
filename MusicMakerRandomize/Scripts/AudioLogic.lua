5d369088c0b6b4acd59a49654c2f7b7146892e5b
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-05 09:50:50 +0000

    Show currently playing track in description

--- MusicMakerRandomize/Scripts/AudioLogic.lua
+++ MusicMakerRandomize/Scripts/AudioLogic.lua
@@ -11,17 +11,23 @@ ModUtil.Path.Wrap("MusicianMusic", function(base, args)
 			-- Choose only from unlocked tracks
 			for _, songName in ipairs(ScreenData.MusicPlayer.Songs) do
 				if songName ~= "Song_RandomSong" and GameState.WorldUpgrades[songName] then
-					table.insert(availableTracks, WorldUpgradeData[songName].TrackName)
+					table.insert(availableTracks, WorldUpgradeData[songName])
 				end
 			end
 		end
 
 		-- Choose a random song from the available ones, if no other song was chosen yet in this "run"
 		if CurrentRun.MusicMakerRandomizeTrackName == nil then
-			CurrentRun.MusicMakerRandomizeTrackName = availableTracks[RandomInt(1, #availableTracks)]
+			local chosenTrack = availableTracks[RandomInt(1, #availableTracks)]
+			CurrentRun.MusicMakerRandomizeTrackName = chosenTrack.TrackName
+			-- To update the description of the Song_RandomSong to include the friendly name of the chosen track
+			GameState.MusicMakerRandomizeFriendlyPlayingSongString = "{#Prev}Currently playing {#ItalicFormat}" .. GetDisplayName({Text = chosenTrack.Name}) .. "{#Prev}."
 		end
 
 		args.TrackName = CurrentRun.MusicMakerRandomizeTrackName
+	else
+		-- If we are playing a different song, do not display its name in the description, as it won't get updated when the player chooses the random song, which may be confusing
+		GameState.MusicMakerRandomizeFriendlyPlayingSongString = ""
 	end
 
 	base(args)
--- MusicMakerRandomize/Scripts/MusicPlayerLogic.lua
+++ MusicMakerRandomize/Scripts/MusicPlayerLogic.lua
@@ -3,5 +3,6 @@ if not MusicMakerRandomize.Config.Enabled then return end
 ModUtil.Path.Wrap("SelectMusicPlayerItem", function(base, screen, button)
 	-- When the player pauses and plays the random song from the Music Maker's menu, choose a new track to play
 	CurrentRun.MusicMakerRandomizeTrackName = nil
+
 	base(screen, button)
 end)
