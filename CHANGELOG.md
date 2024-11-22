c58bdea88f3601174548f5bfc1f43d7e6dbe4264
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-11-22 22:37:21 +0000

    Fixed music switching when switching rooms (#9)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,15 @@
 # Changelog
 
-## v2.0.0
+## v2.0.1
 
 <!--Releasenotes start-->
+- Fixed: Music changes between rooms if a random track is playing.
+<!--Releasenotes end-->
+
+## v2.0.0
+
 - NEW: Mark songs as favorite and have the Music Maker randomize only from favorites through a new song choice:
 ![Randomize Music Maker Song Choice - Unlock favorites](https://github.com/user-attachments/assets/09733994-5346-44eb-988e-af1f039a8faa)
-<!--Releasenotes end-->
 
 ## v1.0.4
 
--- src/Scripts/AudioLogic.lua
+++ src/Scripts/AudioLogic.lua
@@ -14,6 +14,14 @@ modutil.mod.Path.Wrap("MusicianMusic", function(base, args)
 	-- If the chosen song is Song_RandomSong and the Music Maker not already playing something, choose a random track and save it in the currentRun to reset it only after a run ends
 	-- Similarly, for the Song_RandomSongFavorites
 	if args.TrackName == "Song_RandomSongTrack" or args.TrackName == "Song_RandomSongFavoritesTrack" then
+		-- If we are playing the same type of random track as before, and we have already chosen a proper track in this run - play the same track and return
+		if (game.CurrentRun.MusicMakerRandomizerTrackType == args.TrackName and game.CurrentRun.MusicMakerRandomizerTrackName ~= nil) then
+			args.TrackName = game.CurrentRun.MusicMakerRandomizerTrackName
+			base(args)
+			return
+		end
+
+		-- Else, choose a new random track
 		local availableTracks = {}
 
 		if game.GameState and game.GameState.WorldUpgrades then
@@ -43,9 +51,13 @@ modutil.mod.Path.Wrap("MusicianMusic", function(base, args)
 		-- To update the description of the Song_RandomSong to include the friendly name of the chosen track
 		game.GameState.MusicMakerRandomizerFriendlyPlayingSong = game.GetDisplayName({ Text = chosenTrack.Name })
 
+		-- Choose the current type of track and the chosen track's name in the CurrentRun, so we can re-use it when switching rooms
+		game.CurrentRun.MusicMakerRandomizerTrackName = chosenTrack.TrackName
+		game.CurrentRun.MusicMakerRandomizerTrackType = args.TrackName
+
 		args.TrackName = chosenTrack.TrackName
 	else
-		game.GameState.MusicMakerRandomizerFriendlyPlayingSong = game.GetDisplayName({ Text = game.GameState.MusicPlayerSongName})
+		game.GameState.MusicMakerRandomizerFriendlyPlayingSong = game.GetDisplayName({ Text = game.GameState.MusicPlayerSongName })
 	end
 
 	base(args)
